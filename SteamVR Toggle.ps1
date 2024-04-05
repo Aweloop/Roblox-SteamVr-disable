d1c1966c38cc4dd83602cfffcf56b0ab0a8d3d4e
Robin <76678107+Aweloop@users.noreply.github.com>
2024-04-05 19:36:08 +0200

    Add files via upload

---
+++ SteamVR Toggle.ps1
@@ -0,0 +1,15 @@
+﻿# Put the path to that bitch of a folder here
+$folderPath = "C:\Program Files (x86)\Steam\steamapps\common"
+
+if (Test-Path "$folderPath\SteamVR") {
+    Rename-Item "$folderPath\SteamVR" -NewName "SteamVR_Dis"
+    Write-Host "Folder 'SteamVR' renamed to 'steamVR_Dis'"
+} 
+
+elseif (Test-Path "$folderPath\SteamVR_Dis") {
+    Rename-Item "$folderPath\SteamVR_Dis" -NewName "SteamVR"
+    Write-Host "Folder 'SteamVR_Dis' renamed to 'SteamVR'"
+} 
+else {
+    Write-Host "Neither 'SteamVR' nor 'SteamVR_Dis' folder found. check if you put the right path in this program"
+}
